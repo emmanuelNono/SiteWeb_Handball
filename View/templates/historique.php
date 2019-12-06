da055d7d34ed2413a047a2aea8cdaaee5bb7d653
Dominique SAUVIGNON <dominique21000.pro@gmail.com>
2019-12-06 19:18:27 +0100

    correction d'une faute de frappes sur template

--- View/templates/historique.php
+++ View/templates/historique.php
@@ -2,6 +2,6 @@
 <?php include('includes/header.php'); ?>
 <div id="container">
     <div id="historique" class="col-lg-12">
-        <h1>La petite histoir du club</h1>
+        <h1>La petite histoire du club</h1>
     </div>
 <?php include('includes/footer.php');?>
