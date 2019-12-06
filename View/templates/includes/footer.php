711fc4ddcd84f17908dbf05cfbd5effd6013b203
Dominique SAUVIGNON <dominique21000.pro@gmail.com>
2019-12-06 19:40:42 +0100

    ajout des noms dans le footer avec des feuilles de styles à nous

--- View/templates/includes/footer.php
+++ View/templates/includes/footer.php
@@ -1,2 +1,7 @@
-</body>
+    </div>
+    <footer>
+        Fièrement développé par Ze Numerix<br>
+        (Cécile GALLIOZ, Dominique SAUVIGNON, Emmanuel NONO, Raphaël KOSCHIG)
+    </footer>
+    </body>
 </html>
