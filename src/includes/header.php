ed365f70083b8b083677340c70f2f37514f73c1f
CecileGallioz <58588057+CecileGallioz@users.noreply.github.com>
2019-12-17 15:24:49 +0100

    Modification NavBar

--- src/includes/header.php
+++ src/includes/header.php
@@ -36,7 +36,7 @@
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Galeries</a>
+                    <a class="nav-link" href="../body/galerie.php">Galeries</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
