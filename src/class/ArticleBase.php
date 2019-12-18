1f080b47155352a0fdb1abda51b26fc5d0cca1ba
RaphaelKoschig <52168525+RaphaelKoschig@users.noreply.github.com>
2019-12-18 10:50:12 +0100

    Ajoute commentaires à class ArticleBase

--- src/class/ArticleBase.php
+++ src/class/ArticleBase.php
@@ -3,34 +3,23 @@
 
 class ArticleBase
 {
+    // Récupère tous les articles qui ne sont pas supprimés (deleted_at)
     public function getArticlesActifs($db){
         $sql_aa = "SELECT * FROM article where art_deleted_at is null";
         $rs_aa = $db->query($sql_aa);
         return $rs_aa;
     }
 
+    // Récupère toutes les infos d'un article
     public function getArticle($db,$id){
         $o_ra = $db->prepare("SELECT * FROM article where art_id = :id");
         $o_ra->bindParam(':id',$id);
         $o_ra->execute();
         $row = $o_ra->fetchall();
         return $row;
-
-        /*
-        if ($o_ra->execute())
-        {
-            while ($row = $o_ra->fetchall())
-            {
-                return $row;
-            }
-        }
-        else {
-            // utilisateur inconnu
-            header("location:login.php?mes=error");
-        }
-        */
     }
 
+    // Récupère les 3 derniers articles
     public function get3DerniersArticles($db){
         $sql_3a = "SELECT * FROM article ORDER BY art_id DESC LIMIT 3";
         $rs_3a = $db->query($sql_3a);
@@ -38,6 +27,7 @@ class ArticleBase
         return $derniersArticles;
     }
 
+    // Récupère la photo d'un article grâce à son id
     public function getPhotoDeArticle($db,$id){
         $o_rpa = $db->prepare("SELECT pho_nom FROM photo
                                INNER JOIN article
