11ec5b7fb6e0ae0c9d4f0b6dfeb9a55db73a2927
RaphaelKoschig <52168525+RaphaelKoschig@users.noreply.github.com>
2019-12-20 11:02:39 +0100

    Revert "Corrige les fichiers untracked pour gitignore"
    This reverts commit 3085d914182b0b1aa787b2b8d93ba6a067c9ba6d.

---
+++ src/includes/Database.php
@@ -0,0 +1,31 @@
+<?php
+// class_database.php
+class Database
+{
+    var $baseName = "";
+    var $baseUser = "";
+    var $userPassW = "";
+    var $hostName = "";
+    var $connection = "";
+
+    // paramètres passée : valeur par défaut
+    function __construct($base = "bd_hbck", $user = "hbck", $password = "hbck-serfa#67", $host = "localhost")
+    {
+        $this->baseName = $base;
+        $this->baseUser = $user;
+        $this->userPassW = $password;
+        $this->hostName = $host;
+    }
+
+    // stockage de la connexion
+    function makeConnect()
+    {
+        $dsn = 'mysql:host=localhost;dbname=' . $this->baseName . ";charset=UTF8";
+        try {
+            $this->connection = new PDO($dsn, $this->baseUser, $this->userPassW);
+        } catch (PDOException $e) {
+            echo 'Connexion échouée : ' . $e->getMessage();
+        }
+        return $this->connection;
+    }
+}
