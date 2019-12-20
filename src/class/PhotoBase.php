8580861f676649e9b0840841b7635ac1b1e5d6e1
Dominique SAUVIGNON <dominique21000.pro@gmail.com>
2019-12-20 19:01:42 +0100

    photo suite

--- src/class/PhotoBase.php
+++ src/class/PhotoBase.php
@@ -1,10 +1,12 @@
 <?php
 
 
-class GalerieBase
+class PhotoBase
 {
     public function getPhotosActives($db){
-        $sql_pa = "SELECT * FROM photo where pho_deleted_at is null";
+        $sql_pa = "SELECT * FROM photo ";
+        $sql_pa .= " inner join album on photo.pho_album_id = album.alb_id ";
+        $sql_pa .= " where pho_deleted_at is null" ;
         $rs_pa = $db->query($sql_pa);
         return $rs_pa;
     }
@@ -24,16 +26,31 @@ class GalerieBase
         $rs_maxi = $db->query($sql_maxi);
         foreach ( $rs_maxi as $max)
         {
-            echo "max : ". $max['maxi'];
             $res = $max['maxi'];
         }
         return $res;
     }
 
     public function addPhoto($db, $data){
-        $sql_ajout = "INSERT INTO photo (pho_id, pho_nom, pho_album, pho_created_at) VALUES ";
+        $sql_ajout = "INSERT INTO photo (pho_id, pho_nom, pho_album_id, pho_created_at) VALUES ";
         $sql_ajout .= " (:id, :nom, :album, now() );";
         $o_rq_prep = $db->prepare($sql_ajout);
+        $o_rq_prep->execute($data);
+        return $o_rq_prep;
+    }
+
+    public function updatePhoto($db, $data){
+        $sql_modif = "UPDATE photo SET pho_nom =:nom, pho_album_id =:album, pho_updated_at = now() ";
+        $sql_modif .= " WHERE pho_id =:id";
+        $o_rq_prep = $db->prepare($sql_modif);
+        $o_rq_prep->execute($data);
+        return $o_rq_prep;
+    }
+
+    public function updateAlbum($db, $data){
+        $sql_modif = "UPDATE photo SET pho_album_id =:album, pho_updated_at = now() ";
+        $sql_modif .= " WHERE pho_id =:id";
+        $o_rq_prep = $db->prepare($sql_modif);
         $o_rq_prep->execute($data);
         return $o_rq_prep;
     }
