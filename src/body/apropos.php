37ac543c232a0dbb3cd7d13e64b068550c9b277a
CecileGallioz <58588057+CecileGallioz@users.noreply.github.com>
2020-01-31 11:30:16 +0100

    carte

--- src/body/apropos.php
+++ src/body/apropos.php
@@ -12,7 +12,12 @@ include('../includes/header.php')
 <div class="container col justify-content-center">
   <div class="col justify-content-center">
   <h3>Notre club</h3>
-    <p>L'entente</p>
+    <p>Handball Club (HBCK)</p>
+    <p>HBC KINGERSHEIM</br>Espace Coubertin</br>Rue Pierre de Coubertin</br>BP 44</br>
+      68261 KINGERSHEIM</br>
+      <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=7.32314944267273%2C47.78517734906292%2C7.333191633224487%2C47.790280954388294&amp;layer=mapnik&amp;marker=47.787732182408064%2C7.328174829744967" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=47.78773&amp;mlon=7.32817#map=17/47.78773/7.32817">Afficher une carte plus grande</a></small>
+
+</p>
   </div>
 
   <div class="col justify-content-center">
