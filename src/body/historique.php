06423386069ab3fe7b3b4c69973a5dbac3d98ac3
CecileGallioz <58588057+CecileGallioz@users.noreply.github.com>
2020-01-31 11:01:03 +0100

    Ppt

--- src/body/historique.php
+++ src/body/historique.php
@@ -18,7 +18,7 @@ include('../includes/header.php')
 <p>Yoo ch'ai lu dans les DNA que le Racing a encore perdu contre Oberschaeffolsheim. Verdammi et moi ch'avais donc parié deux knacks et une flammekueche. Ah so ? T'inquiète, ch'ai ramené du schpeck, du chambon, un kuglopf et du schnaps dans mon rucksack. Allez, s'guelt ! Wotch a kofee avec ton bibalaekaess et ta wurscht ? Yeuh non che suis au réchime, je ne mange plus que des Grumbeere light et che fais de la chym avec Chulien. Tiens, un rottznoz sur le comptoir.</p>
 <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci vielmols mais che dois partir à la Coopé de Truchtersheim acheter des mänele et des rossbolla pour les gamins. Hopla tchao bissame ! Consectetur adipiscing elit</p>
 
-
+<img src="../../resources/galerie/Choucroute.jpg" class=""/>
 
 </div>
 <br/>
