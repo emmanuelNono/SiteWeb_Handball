0df44636329900f4860cf5daa4b9a0e33c17b8e3
CecileGallioz <58588057+CecileGallioz@users.noreply.github.com>
2020-01-31 11:35:37 +0100

    apropos3

--- src/body/apropos.php
+++ src/body/apropos.php
@@ -14,9 +14,9 @@ include('../includes/header.php')
   <h3>Notre club</h3>
     <p>Handball Club (HBCK)</p>
     <p>HBC KINGERSHEIM</br>Espace Coubertin</br>Rue Pierre de Coubertin</br>BP 44</br>
-      68261 KINGERSHEIM</br>
+      68261 KINGERSHEIM</br><br/>
       <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=7.32314944267273%2C47.78517734906292%2C7.333191633224487%2C47.790280954388294&amp;layer=mapnik&amp;marker=47.787732182408064%2C7.328174829744967" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=47.78773&amp;mlon=7.32817#map=17/47.78773/7.32817">Afficher une carte plus grande</a></small>
-
+<br/>
 </p>
   </div>
 
